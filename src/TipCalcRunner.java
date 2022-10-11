5040e95747b7d0eb07f0c520d840a37fd007313e
Raj Biswas <rajb4@nycstudents.net>
2022-10-10 21:12:46 -0400

    TipCalc Redo

--- src/TipCalcRunner.java
+++ src/TipCalcRunner.java
@@ -3,7 +3,7 @@ import java.text.DecimalFormat;
 
 public class TipCalcRunner {
     public static void main(String [] args) {
-        DecimalFormat formatter = new DecimalFormat("#,##");
+        DecimalFormat formatter = new DecimalFormat("#.##");
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Hello, welcome to the tip calculator!");
@@ -17,8 +17,32 @@ public class TipCalcRunner {
         TipCalc calc = new TipCalc(people,tip);
 
         System.out.println("Enter a cost (Press -1 when done): ");
+        double cost = scan.nextDouble();
+
+        while (cost != -1) {
+            calc.addfood(cost);
+            System.out.print("Enter a cost (Press -1 when done): ");
+            cost = scan.nextDouble();
+        }
+
+
+        String formatTotalCost = formatter.format(calc.getTotalBillBeforeTip());
+        String formatTip = formatter.format(calc.getTipPercentage());
+        String formatTotalTip = formatter.format(calc.tipAmount());
+        String formatTipBill = formatter.format(calc.totalBill());
+        String formatPersonCost = formatter.format(calc.PerPersonTotalCost());
+        String formatPersonTip = formatter.format(calc.PerPersonCostBeforeTip());
+        String formatPersonTotal = formatter.format(calc.PerPersonTotalCost());
+
+        System.out.println("Total bill before tip: " + formatTotalCost);
+        System.out.println("Tip percentage: " + formatTip);
+        System.out.println("Total tip: " + formatTotalTip);
+        System.out.println("Total bill with the tip: " + formatTipBill);
+        System.out.println("Cost per person without tip: " + formatPersonCost);
+        System.out.println("The tip per person: " + formatPersonTip);
+        System.out.println("Total cost per person: " + formatPersonTotal);
 
         }
     }
-}
+
 
